2c2e1c46079f99b9b04da6e5b5bafce1bb1a251e
Gabriel Lau <gabrielslau@gmail.com>
2015-12-01 16:45:01 -0300

    implementacao da classe No

--- Java/ArvoreGenerica/src/main/java/ifrn/tads/estruturadedados/binarytree/No.java
+++ Java/ArvoreGenerica/src/main/java/ifrn/tads/estruturadedados/binarytree/No.java
@@ -1,33 +1,43 @@
 package ifrn.tads.estruturadedados.binarytree;
 
 import java.util.Iterator;
+import java.util.Vector;
 
 public class No implements INo {
+    private Object o;
+    private No pai;
+    private Vector<INo> filhos = new Vector<INo>();
+
+    public No(No pai, Object o) {
+        this.pai = pai;
+        this.o = o;
+    }
+
     public Object element() {
-        return null;
+        return o;
     }
 
     public INo parent() {
-        return null;
+        return pai;
     }
 
     public void setElement(Object o) {
-
+        this.o = o;
     }
 
     public void addChild(INo o) {
-
+        filhos.add(o);
     }
 
     public void removeChild(INo o) {
-
+        filhos.remove(o);
     }
 
     public int childrenNumber() {
-        return 0;
+        return filhos.size();
     }
 
     public Iterator children() {
-        return null;
+        return filhos.iterator();
     }
 }
