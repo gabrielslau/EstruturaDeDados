470f9ebd50a9f4b0955cd0dd63064f944f8fbcdd
Gabriel Lau <gabriel@linkest.net>
2016-07-19 01:20:34 -0300

    implementação de árvore binária de pesquisa com testes unitários (100% coverage)

---
+++ Java/ArvoreGenerica/src/main/java/ifrn/tads/estruturadedados/binarysearchtree/BinarySearchTree.java
@@ -0,0 +1,193 @@
+package ifrn.tads.estruturadedados.binarysearchtree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class BinarySearchTree<T extends Comparable<T>> {
+
+    private Node<T> root;
+    private ArrayList<T> elements;
+
+    public BinarySearchTree() {
+        this.root = null;
+        elements = new ArrayList<T>();
+    }
+
+    public BinarySearchTree(List<T> elementsToAdd) {
+        this();
+
+        for (T element : elementsToAdd) {
+            insert(element);
+        }
+    }
+
+    public ArrayList<T> elements() {
+        return elements;
+    }
+
+    public Node<T> root() {
+        return root;
+    }
+
+    public Node<T> find(T targetData) {
+        Node<T> current = root;
+
+        while (current != null) {
+            int comparator = current.data.compareTo(targetData);
+
+            if (comparator == 0) {
+                return current;
+            } else if (comparator > 0) {
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * @param nodeToDelete node to delete
+     * @return the smallest node of the right child of the node which is about to be deleted
+     */
+    public Node<T> findSuccessor(Node<T> nodeToDelete) {
+        Node<T> successor = null;
+        Node<T> current = nodeToDelete.right;
+
+        while (current != null) {
+            successor = current;
+            current = current.left;
+        }
+
+        return successor;
+    }
+
+    public void insert(T data) {
+        root = insert(root, data);
+    }
+
+    protected Node<T> insert(Node<T> nodeTarget, T data) {
+        if (nodeTarget == null) {
+            return new Node<T>(data);
+        }
+
+        if (nodeTarget.data.compareTo(data) >= 0) {
+            boolean isLeaf = nodeTarget.left == null;
+            nodeTarget.left = insert(nodeTarget.left, data);
+
+            if (isLeaf) {
+                nodeTarget.left.parent = nodeTarget;
+            }
+        } else {
+            boolean isLeaf = nodeTarget.right == null;
+            nodeTarget.right = insert(nodeTarget.right, data);
+
+            if (isLeaf) {
+                nodeTarget.right.parent = nodeTarget;
+            }
+        }
+
+        return nodeTarget;
+    }
+
+    public void displayPreOrder(Node<T> root) {
+        if (root == null) return;
+
+        elements.add(root.data);
+        displayPreOrder(root.left);
+        displayPreOrder(root.right);
+    }
+
+    public void displayPostOrder(Node<T> root) {
+        if (root == null) return;
+
+        displayPostOrder(root.left);
+        displayPostOrder(root.right);
+        elements.add(root.data);
+    }
+
+    public void displayInOrder(Node<T> root) {
+        if (root == null) return;
+
+        displayInOrder(root.left);
+        elements.add(root.data);
+        displayInOrder(root.right);
+    }
+
+    public boolean delete(T data) {
+        Node<T> node = find(data);
+
+        if (node == null) return false;
+
+        // Case 1: if node to be deleted is a leaf node
+        if (node.numberOfChildren() == 0) {
+            if (node == root) {
+                root = null;
+            } else if (node.isLeftChild()) {
+                node.parent.left = null;
+            } else {
+                node.parent.right = null;
+            }
+        }
+
+        // Case 2: if node to be deleted has only one child
+        else if (node.numberOfChildren() == 1) {
+            if (node.hasLeftChild()) {
+                node.left.parent = node.parent;
+
+                if (node.isLeftChild()) {
+                    node.parent.left = node.left;
+                } else {
+                    node.parent.right = node.left;
+                }
+            } else {
+                node.right.parent = node.parent;
+
+                if (node.isLeftChild()) {
+                    node.parent.left = node.right;
+                } else {
+                    node.parent.right = node.right;
+                }
+            }
+        }
+
+        // Case 3: if node to be deleted has two childrens
+        else {
+            Node<T> nodeSuccessor = findSuccessor(node);
+
+            if (node == root) {
+                root = nodeSuccessor;
+            } else if (node.isLeftChild()) {
+                node.parent.left = nodeSuccessor;
+            } else {
+                node.parent.right = nodeSuccessor;
+            }
+
+            if (nodeSuccessor.isLeftChild()) {
+                nodeSuccessor.parent.left = null;
+            } else {
+                nodeSuccessor.parent.right = null;
+            }
+
+            nodeSuccessor.left = node.left;
+            nodeSuccessor.right = node.right;
+            nodeSuccessor.parent = node.parent;
+
+            if (node.hasLeftChild()) {
+                nodeSuccessor.left.parent = nodeSuccessor;
+            }
+
+            if (node.hasRightChild()) {
+                nodeSuccessor.right.parent = nodeSuccessor;
+            }
+
+            nodeSuccessor.parent = node.parent;
+
+            // Move to trash
+            node = null;
+        }
+
+        return true;
+    }
+}
---
+++ Java/ArvoreGenerica/src/test/java/ifrn/tads/estruturadedados/binarysearchtree/BinarySearchTreeTest.java
@@ -0,0 +1,189 @@
+package ifrn.tads.estruturadedados.binarysearchtree;
+
+import junit.framework.TestCase;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class BinarySearchTreeTest extends TestCase {
+    private BinarySearchTree<Integer> tree;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        tree = new BinarySearchTree<Integer>(Arrays.asList(4, 2, 6, 1, 3, 5));
+
+        /*
+             4
+            / \
+           /   \
+          2     6
+         / \   / \
+        1   3 5   7
+
+
+        */
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+
+        BTreePrinter.printNode(tree.root());
+
+        tree = null;
+    }
+
+    public void testInsertAndDisplayPostOrder() {
+        tree.displayPostOrder(tree.root());
+        ArrayList<Integer> expected = new ArrayList<Integer>(Arrays.asList(1, 3, 2, 5, 6, 4));
+
+        assertEquals(expected, tree.elements());
+    }
+
+    public void testInsertAndDisplayPreOrder() {
+        tree.displayPreOrder(tree.root());
+        ArrayList<Integer> expected = new ArrayList<Integer>(Arrays.asList(4, 2, 1, 3, 6, 5));
+
+        assertEquals(expected, tree.elements());
+    }
+
+    public void testInsertAndDisplayInOrder() {
+        tree.displayInOrder(tree.root());
+        ArrayList<Integer> expected = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6));
+
+        assertEquals(expected, tree.elements());
+    }
+
+    public void testFindMustReturnNodeWithSameData() {
+        Integer expected = 4;
+        Node<Integer> result = tree.find(expected);
+
+        assertEquals(expected, result.data);
+    }
+
+    public void testFindMustReturnNull() {
+        Node<Integer> result = tree.find(9000);
+        assertEquals(null, result);
+    }
+
+    public void testNodeParent() {
+        Integer expected4 = 4;
+        Integer expected2 = 2;
+        Integer expected6 = 6;
+
+        assertNull(tree.find(4).parent);
+        assertEquals(expected4, tree.find(2).parent.data);
+        assertEquals(expected4, tree.find(6).parent.data);
+        assertEquals(expected2, tree.find(1).parent.data);
+        assertEquals(expected2, tree.find(3).parent.data);
+        assertEquals(expected6, tree.find(5).parent.data);
+    }
+
+    public void testDeleteRootWithNoChildren() {
+        tree = new BinarySearchTree<Integer>(Collections.singletonList(4));
+
+        assertTrue(tree.delete(4));
+
+        tree.displayInOrder(tree.root());
+        ArrayList<Integer> expected = new ArrayList<Integer>();
+
+        assertEquals(expected, tree.elements());
+    }
+
+    public void testDeleteRootWithBothChild() {
+        tree.insert(7);
+
+        assertTrue(tree.delete(4));
+
+        tree.displayInOrder(tree.root());
+        ArrayList<Integer> expected = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 5, 6, 7));
+
+        assertEquals(expected, tree.elements());
+    }
+
+    public void testDeleteRightNodeWithLonelinessLeftChildNode() {
+        assertTrue(tree.delete(6));
+
+        tree.displayInOrder(tree.root());
+        ArrayList<Integer> expected = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5));
+
+        assertEquals(expected, tree.elements());
+    }
+
+    public void testDeleteRightNodeWithLonelinessRightChildNode() {
+        tree.insert(7);
+        assertTrue(tree.delete(5));
+        // Delete the node with a right child (7)
+        assertTrue(tree.delete(6));
+
+        tree.displayInOrder(tree.root());
+        ArrayList<Integer> expected = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 7));
+
+        assertEquals(expected, tree.elements());
+    }
+
+    public void testDeleteLeftNodeWithLonelinessLeftChildNode() {
+        assertTrue(tree.delete(3));
+        // Delete the node with a left child (1)
+        assertTrue(tree.delete(2));
+
+        tree.displayInOrder(tree.root());
+        ArrayList<Integer> expected = new ArrayList<Integer>(Arrays.asList(1, 4, 5, 6));
+
+        assertEquals(expected, tree.elements());
+    }
+
+    public void testDeleteLeftNodeWithLonelinessRightChildNode() {
+        assertTrue(tree.delete(1));
+        // Delete the node with a right child (3)
+        assertTrue(tree.delete(2));
+
+        tree.displayInOrder(tree.root());
+        ArrayList<Integer> expected = new ArrayList<Integer>(Arrays.asList(3, 4, 5, 6));
+
+        assertEquals(expected, tree.elements());
+    }
+
+    public void testDeleteLeftChildNode() {
+        assertTrue(tree.delete(2));
+
+        tree.displayInOrder(tree.root());
+        ArrayList<Integer> expected = new ArrayList<Integer>(Arrays.asList(1, 3, 4, 5, 6));
+
+        assertEquals(expected, tree.elements());
+    }
+
+    public void testDeleteRightChildNode() {
+        tree.insert(7);
+
+        assertTrue(tree.delete(6));
+
+        tree.displayInOrder(tree.root());
+        ArrayList<Integer> expected = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 7));
+
+        assertEquals(expected, tree.elements());
+    }
+
+    public void testDeleteLeftLeafNode() {
+        assertTrue(tree.delete(1));
+
+        tree.displayInOrder(tree.root());
+        ArrayList<Integer> expected = new ArrayList<Integer>(Arrays.asList(2, 3, 4, 5, 6));
+
+        assertEquals(expected, tree.elements());
+        assertNull(tree.find(2).left);
+    }
+
+    public void testDeleteRightLeafNode() {
+        assertTrue(tree.delete(3));
+
+        tree.displayInOrder(tree.root());
+        ArrayList<Integer> expected = new ArrayList<Integer>(Arrays.asList(1, 2, 4, 5, 6));
+
+        assertEquals(expected, tree.elements());
+        assertNull(tree.find(2).right);
+    }
+}
