80d0375b535ad426525902293c34101f1a2fd18a
Gabriel Lau <gabriel@linkest.net>
2016-10-19 05:12:50 -0300

    mudança na nomenclatura do método de fator de balanceamento

--- Java/ArvoreGenerica/src/main/java/ifrn/tads/estruturadedados/tree/avl/AvlTree.java
+++ Java/ArvoreGenerica/src/main/java/ifrn/tads/estruturadedados/tree/avl/AvlTree.java
@@ -11,7 +11,7 @@ class AvlTree<T extends Comparable<T>> extends BinarySearchTree<T> {
         super(elementsToAdd);
     }
 
-    public int getBalance(Node<T> n) {
+    public int getBalanceFactor(Node<T> n) {
         if (n != null) {
             return (getHeight((Node<T>) n.left) - getHeight((Node<T>) n.right));
         }
@@ -25,34 +25,34 @@ class AvlTree<T extends Comparable<T>> extends BinarySearchTree<T> {
         return 0;
     }
 
-    public Node<T> rightRotate(Node<T> y) {
-        Node<T> x = (Node<T>) y.left;
-        Node<T> T2 = (Node<T>) x.right;
+    public Node<T> rightRotate(Node<T> nodeTarget) {
+        Node<T> rotatedNode = (Node<T>) nodeTarget.left;
+        Node<T> tempNode = (Node<T>) rotatedNode.right;
 
         // Rotation
-        x.right = y;
-        y.left = T2;
+        rotatedNode.right = nodeTarget;
+        nodeTarget.left = tempNode;
 
         // update their heights
-        x.height = Math.max(getHeight((Node<T>) x.left), getHeight((Node<T>) x.right)) + 1;
-        y.height = Math.max(getHeight((Node<T>) y.left), getHeight((Node<T>) y.right)) + 1;
+        rotatedNode.height = Math.max(getHeight((Node<T>) rotatedNode.left), getHeight((Node<T>) rotatedNode.right)) + 1;
+        nodeTarget.height = Math.max(getHeight((Node<T>) nodeTarget.left), getHeight((Node<T>) nodeTarget.right)) + 1;
 
-        return x;
+        return rotatedNode;
     }
 
-    public Node<T> leftRotate(Node<T> x) {
-        Node<T> y = (Node<T>) x.right;
-        Node<T> T2 = (Node<T>) y.left;
+    public Node<T> leftRotate(Node<T> nodeTarget) {
+        Node<T> rotatedNode = (Node<T>) nodeTarget.right;
+        Node<T> tempNode = (Node<T>) rotatedNode.left;
 
         // Rotation
-        y.left = x;
-        x.right = T2;
+        rotatedNode.left = nodeTarget;
+        nodeTarget.right = tempNode;
 
         // update their heights
-        x.height = Math.max(getHeight((Node<T>) x.left), getHeight((Node<T>) x.right)) + 1;
-        y.height = Math.max(getHeight((Node<T>) y.left), getHeight((Node<T>) y.right)) + 1;
+        nodeTarget.height = Math.max(getHeight((Node<T>) nodeTarget.left), getHeight((Node<T>) nodeTarget.right)) + 1;
+        rotatedNode.height = Math.max(getHeight((Node<T>) rotatedNode.left), getHeight((Node<T>) rotatedNode.right)) + 1;
 
-        return y;
+        return rotatedNode;
     }
 
     protected AbstractNode<T> insert(AbstractNode<T> nodeTarget, T data) {
@@ -73,30 +73,30 @@ class AvlTree<T extends Comparable<T>> extends BinarySearchTree<T> {
         // update the node height
         nodeTarget.height = Math.max(leftHeight, rightHeight) + 1;
 
-        int balance = getBalance(nodeTarget);
+        int balanceFactor = getBalanceFactor(nodeTarget);
 
         // Left Rotate
         //if (balDiff > 1 && data < node.left.data) {
-        if (balance > 1 && nodeTarget.left.data.compareTo(data) >= 0) {
+        if (balanceFactor > 1 && nodeTarget.left.data.compareTo(data) >= 0) {
             return rightRotate(nodeTarget);
         }
 
         // Right Rotate
-        //if (balance < -1 && data > nodeTarget.right.data) {
-        if (balance < -1 && nodeTarget.right.data.compareTo(data) < 0) {
+        //if (balanceFactor < -1 && data > nodeTarget.right.data) {
+        if (balanceFactor < -1 && nodeTarget.right.data.compareTo(data) < 0) {
             return leftRotate(nodeTarget);
         }
 
         // Left Right Rotate
-        //if (balance > 1 && data > nodeTarget.left.data) {
-        if (balance > 1 && nodeTarget.left.data.compareTo(data) < 0) {
+        //if (balanceFactor > 1 && data > nodeTarget.left.data) {
+        if (balanceFactor > 1 && nodeTarget.left.data.compareTo(data) < 0) {
             nodeTarget.left = leftRotate((Node<T>) nodeTarget.left);
             return rightRotate(nodeTarget);
         }
 
         // Right Left Rotate
-        //if (balance < -1 && data < nodeTarget.right.data) {
-        if (balance < -1 && nodeTarget.right.data.compareTo(data) >= 0) {
+        //if (balanceFactor < -1 && data < nodeTarget.right.data) {
+        if (balanceFactor < -1 && nodeTarget.right.data.compareTo(data) >= 0) {
             nodeTarget.right = rightRotate((Node<T>) nodeTarget.right);
             return leftRotate(nodeTarget);
         }
